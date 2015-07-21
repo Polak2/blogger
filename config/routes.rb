334c98f28b7782eb2beaa0085e944757257222ea
Polak2 <czesio231@gmail.com>
2015-07-21 18:16:46 +0200

    tagged for articles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  get '/tags', to: 'tags#index', as: 'tags'
+
   root 'articles#index'
   resources :articles do
   	resources :comments
   end
+  resources :tags
+  
 end
